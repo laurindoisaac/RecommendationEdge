dea27f374e0347dafee3a8b8436b6ed5f8ffd3a0
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 10:07:49 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/recommendation/main.go
@@ -0,0 +1,23 @@
+// cmd/recommendation/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "recommendation/internal/recommendation"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := recommendation.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
